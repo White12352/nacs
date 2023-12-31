723e86bb48a3e88f6bf48dd9c857d0c5a7368b9a
White12352 <86940955+White12352@users.noreply.github.com>
2023-12-31 12:45:34 +0800

    Update core.sh

--- buildScript/lib/core.sh
+++ buildScript/lib/core.sh
@@ -71,7 +71,7 @@ cd ..
 go mod tidy
 #cd ../nacs/libcore/.build/src
 #go mod tidy
-cd ../libcore
+cd ../../libcore
 awk '{if(index($0, "// replace github.com/sagernet/sing =>") > 0) $0 = "replace github.com/sagernet/sing => ../../sing"}1' go.mod > temp_file && mv -f temp_file go.mod
 awk '{if ($0 ~ /\/\/ replace github\.com\/sagernet\/sing-dns => \.\.\/\.\.\/sing-dns/) {print "// replace github.com/sagernet/sing-dns => ../../sing-dns"; print ""; print "replace github.com/sagernet/sing-quic => ../../sing-quic"} else {print $0}}' go.mod > temp_file && mv -f temp_file go.mod
 go mod tidy
